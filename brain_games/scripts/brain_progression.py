6a2b2c7705b058f5dfeb3f9ead933b258aa48125
Дарья Степанова <step.dasha.2000@gmail.com>
2025-02-20 21:00:21 +0300

    Изменения в brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,8 @@
 import random
 
-def generate_progression(length=None, start=1, step=2):
+def generate_progression(length=None, start=1, step=2): 
+    if length is None:
+        length = random.randint(5, 10)
     return [start + step * i for i in range(length)]
 
 def hide_element(progression):
@@ -15,16 +17,21 @@ def main():
     print(f"Hello, {name}!")
     print("What number is missing in the progression?")
 
-    progression = generate_progression()
-    hidden_value, displayed_progression = hide_element(progression)
+    for _ in range(3):
+        progression = generate_progression()
+        hidden_value, displayed_progression = hide_element(progression)
 
-    print("Question:", ' '.join(map(str, displayed_progression)))
-    user_answer = input("Your answer: ")
+        print("Question:", ' '.join(map(str, displayed_progression)))
+        user_answer = input("Your answer: ")
 
-    if int(user_answer) == hidden_value:
-        print("Correct!")
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{hidden_value}'.")
-        print(f"Let's try again, {name}!")
-
+        if int(user_answer) == hidden_value:
+            print("Correct!")
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{hidden_value}'.")
+            print(f"Let's try again, {name}!")
+            return
     print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
