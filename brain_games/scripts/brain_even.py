2867f973a9733a01cb4a6f913aab2f4027f059b8
Дарья Степанова <step.dasha.2000@gmail.com>
2025-02-27 23:18:05 +0300

    Корректировка кода в brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,14 @@
 import random
 
-def even_game_logic():
+def is_even_game_logic():
     number = random.randint(1, 100)
     question = str(number)
     correct_answer = "yes" if number % 2 == 0 else "no"
     return question, correct_answer
 
+def main():
+    play_game(is_even_game_logic)
+
 if __name__ == "__main__":
-    play_game(even_game_logic)
+    main()
 
