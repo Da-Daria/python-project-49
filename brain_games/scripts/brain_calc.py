57a4ce22f39a94784e155621c9cfc7cbaf50362c
Дарья Степанова <step.dasha.2000@gmail.com>
2025-03-03 00:09:03 +0300

    Изменеия в brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,13 +2,11 @@ import random
 
 from brain_games.game_engine import play_game
 
-def main():
+def calculator_game_logic():
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     operation = random.choice(['+', '-', '*'])
     question = f"{num1} {operation} {num2}"
-
-    print("What is the result of the expression?")
 
     if operation == '+':
         answer = num1 + num2
@@ -17,8 +15,8 @@ def main():
     else:
         answer = num1 * num2
 
-    return question, answer
+    return question, str(answer)
 
 if __name__ == "__main__":
-    play_game(main)
+    play_game(calculator_game_logic)
 
